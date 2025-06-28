8207905c83a625ff5a6ec8a1b1f9416fcea33aa8
Hong yongjae <developsvai5096@gmail.com>
2025-06-28 23:32:20 +0900

    add config.py

---
+++ config/config.py
@@ -0,0 +1,8 @@
+# /opt/tinypilot/config.py
+
+DEBUG = False
+SECRET_KEY = 'smt32f734o@'
+FLASK_RUN_HOST = '0.0.0.0'
+KEYBOARD_PATH = "/dev/hidg0"  # /dev/hidg1
+MOUSE_PATH = "/dev/hidg1"  # /dev/hidg0
+# Add other configuration variables here
