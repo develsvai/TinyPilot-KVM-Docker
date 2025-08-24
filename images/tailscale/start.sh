572b807bba21ec32efe95bcd9b8b98860316cfd2
Hong yongjae <developsvai5096@gmail.com>
2025-08-24 20:20:58 +0900

    Refactor : nginx-ingress 주석 처리-> 대신 테일스케일 사용, 일부 파일명 들 변경

--- images/tailscale/start.sh
+++ images/tailscale/start.sh
@@ -31,8 +31,8 @@ nginx &
 sleep 3
 
 # 4. Tailscale의 serve 기능을 사용해 HTTPS(443) 트래픽을 내부 9090 포트로 프록시
-#echo "Starting tailscale serve..."
-#tailscale serve --bg --tcp 80 localhost:9090
+echo "Starting tailscale serve..."
+tailscale serve --bg --tcp 80 localhost:9090
 
 # 5. 스크립트(컨테이너)가 종료되지 않도록 유지
 echo "Setup complete. Keeping the container running..."
